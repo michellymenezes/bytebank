f1598e717ae13710d300228887aeee04567f3b91
Michelly Menezes <martha.michelly@gmail.com>
2020-09-15 16:46:28 -0300

    Create classes to add static content

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,47 @@ void main() => runApp(
           appBar: AppBar(
             title: Text("Transfers"),
           ),
+          body: TransferList(),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
           ),
         ),
       ),
     );
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransferItem(Transfer(100.0, 1000)),
+        TransferItem(Transfer(200, 2000)),
+        TransferItem(Transfer(300, 3000)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer; //the _ makes the property private
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+}
