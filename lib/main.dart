09998223b4bad3ed14212fa3c5de1f344d53665d
Michelly Menezes <martha.michelly@gmail.com>
2020-09-18 17:43:44 -0300

    Load image

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,39 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TransferList(),
+      home: Dashboard(),
       theme: ThemeData(
           primaryColor: Colors.green[900],
           accentColor: Colors.blueAccent[700],
           buttonTheme: ButtonThemeData(
               buttonColor: Colors.blueAccent[700],
               textTheme: ButtonTextTheme.primary)),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Column(
+        children: [
+          Image.asset("images/bytebank_logo.png"),
+          Container(
+            color: Colors.green,
+            child: Column(
+              children: [
+                Icon(Icons.people),
+                Text("Contacts")
+              ],
+            ),
+          ),
+          Text("Button")
+        ],
+      ),
     );
   }
 }
