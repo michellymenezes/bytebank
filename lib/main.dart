8d4f758f15d3e64559dc90278ef906cc45b27ac4
Michelly Menezes <martha.michelly@gmail.com>
2020-09-21 16:23:27 -0300

    Connect screens with button action

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // import 'package:flutter/cupertino.dart';
+import 'package:bytebank/screens/transfer/dashboard.dart';
 import 'package:bytebank/screens/transfer/list.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,7 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ContactList(),
+      home: Dashboard(),
       theme: ThemeData(
           primaryColor: Colors.green[900],
           accentColor: Colors.blueAccent[700],
--- lib/screens/transfer/dashboard.dart
+++ lib/screens/transfer/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/main.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -17,29 +18,42 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
+            // Wrap in Material
+            child: Material(
               color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
-                  ),
-                  Text(
-                    "Contacts",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.00,
+
+              // Use InkWell
+              child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => ContactList(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    height: 100,
+                    width: 150, // Remove the color property to apply the effect
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(
+                          Icons.people,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                        Text(
+                          "Contacts",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.00,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
+                  )),
             ),
           ),
         ],
