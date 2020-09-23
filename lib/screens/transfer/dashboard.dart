c77453f1cf9062d22703d159084b293d2e50b7fc
Michelly Menezes <martha.michelly@gmail.com>
2020-09-23 17:07:23 -0300

    add new button

--- lib/screens/transfer/dashboard.dart
+++ lib/screens/transfer/dashboard.dart
@@ -18,47 +18,65 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset("images/bytebank_logo.png"),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            // Wrap in Material
-            child: Material(
-              color: Theme.of(context).primaryColor,
+          Row(
+            children: [
+              _FeatureItem("Transfer", Icons.monetization_on),
+              _FeatureItem("Transaction Feed", Icons.description),
+            ],
+          )
 
-              // Use InkWell
-              child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => ContactList(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    height: 100,
-                    width: 150, // Remove the color property to apply the effect
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(
-                          Icons.people,
-                          color: Colors.white,
-                          size: 24.0,
-                        ),
-                        Text(
-                          "Contacts",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.00,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )),
-            ),
-          ),
         ],
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+
+  const _FeatureItem(this.name, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      // Wrap in Material
+      child: Material(
+        color: Theme.of(context).primaryColor,
+
+        // Use InkWell
+        child: InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => ContactList(),
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 100,
+              width: 150, // Remove the color property to apply the effect
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    icon,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.00,
+                    ),
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
